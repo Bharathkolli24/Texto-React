9134330bc9191fad3e26a8f990f033ba8eb3b7b5
Bharathkolli24 <bharathkolli24@gmail.com>
2024-07-23 22:07:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import Alerts from './components/4.Alerts';
+// import About from './components/About';
+import Navbar from './components/1.Navbar';
+import TextForm from './components/2.TextForm';
+import React, { useState } from 'react';
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+
+// let Name = "Bharath";       //if we write <b>Bharath</b> assuming that it'll become bold . It'll not because it'll not import external html..It'll nprint like <b>Bharath</b>    
+function App() {
+  const [mode, setMode] = useState('light');    //Default mode is white. Whether the dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  //This is for Alert messages
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
+  // This is for Dark Mode and Light Mode
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#232442'
+      showAlert("Dark mode enabled", "success")
+      document.title = 'TextUtils - Dark Mode'
+
+      // setInterval(() => {                                         it is used to interrupt the title of website..when you enable this the title will change according to given text in given time interval
+      //   document.title = "TextUtils is Amazing website"
+      // },1000);
+      // setInterval(() => {
+      //   document.title = "Install Now"
+      // },1500);
+
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode enabled", "success")
+      document.title = 'TextUtils - Light Mode'
+
+    }
+  }
+
+
+  return (
+    <>
+      {/* <BrowserRouter> */}
+        <Navbar title="TextUtils" aboutTextUtils="About" mode={mode} toggleMode={toggleMode}/>
+        {/* <Navbar/>        it's for default props*/}
+        <Alerts alert={alert} />
+        <div className="container my-3" mode={mode}>
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}></Route> */}
+            {/* <Route exact path="/" element= {*/} <TextForm heading="Enter text to analyze " mode={mode} showAlert={showAlert}/>  {/*}></Route> */}   {/*We're passing props here , it'll gets from Navbar as we have imported navbar too...We can also change the title here(It is used to reuse the application)...line 2 was automtically written(imported) by itself when we write this*/}
+          {/* </Routes> */}
+        </div>
+      {/* </BrowserRouter> */}
+    </>
+  );
+
+}
+
+export default App;                           
---
+++ src/components/2.TextForm.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'       //{useState} is known as Hook -Hook is nothing but acquiring the properties of classes without creating classes 
+
+export default function TextForm(props) {
+    const handleUpClick = () =>{
+        console.log(text); 
+        let newText = text.toUpperCase();                       //it'll change the text we've written to upper case
+        setText(newText);  
+        props.showAlert("Converted to Uppercase!",  "success")                               
+    }
+    const handleLowerClick = () =>{
+        console.log(text); 
+        let newText = text.toLowerCase();                       //it'll change the text we've written to lower case
+        setText(newText);                  
+        props.showAlert("Converted to Lowercase!",  "success")                               
+    }
+    const handleClearClick = () =>{
+        console.log(text); 
+        let newText = '';                       //it'll change the text we've written to lower case
+        setText(newText);            
+        props.showAlert("CLeared the text",  "success")                               
+
+    }
+    const handleCopy = () => {                         //it'll copy the entire text which we have written written 
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to Clipboard",  "success")                               
+    } 
+    const handleExtraSpaces = () => {                          //it'll remove the extra spaces 
+        let newText = text.split(/[  ]+ /);
+        setText(newText.join(""))
+        props.showAlert("Removed Extra Spaces",  "success")
+    }
+    const handleOnChange = (event) => {
+        // console.log("On Change");                //no need to write this clg
+        setText(event.target.value)
+    }
+
+    const [text,setText] = useState('');   //Here enter text here belongs to Text. And here 2 variables are there text,setText it means when we write something in Text it'll also reflect to setText 
+    // text = 'newtext';          //wrong way to change the state
+    // setText("newText");  //Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{color:props.mode ==='light' ?'#232442':'white'}}>
+            <h1>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode ==='light' ?'white':'grey',color:props.mode ==='light' ?'#232442':'white'}} id="myBox" rows="8"></textarea>    {/*     here we've to listen for handleOnChange and then we've to set the value of it  by using value={text}. And we've put two {{}} because one curly brac e of javascript nd another curly brace is for object in javascript */}
+            </div>    
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}> Convert to UpperCase </button>
+            <button className="btn btn-primary mx-1" onClick={handleLowerClick}> Convert to LowerCase </button>
+            <button className="btn btn-primary mx-1" onClick={handleClearClick}> Clear Text </button>
+            <button className="btn btn-primary mx-1" onClick={handleCopy}> Copy Text </button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}> Remove Extra Spaces </button>
+        </div>
+        <div className="container" style={{color:props.mode ==='light' ?'#232442':'white'}}>
+            <h1 className='mt-4'>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>  {/*  if we want bold letters use <b></b> */}
+            <p>{0.08 * text.split(" ").length} Minutes to read</p>
+            <h2 className='mt-4'>Preview</h2>
+            <p>{text.length>0?text:"Enter something in the textbox above to preview"}</p>
+        </div>
+        </>
+    )
+}
