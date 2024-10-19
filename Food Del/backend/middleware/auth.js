bb75cacbf7cd9816b145846bc529451a87ddab87
Bharathkolli24 <bharathkolli24@gmail.com>
2024-10-19 20:55:31 +0530

    first-commit

--- Food Del/backend/middleware/auth.js
+++ Food Del/backend/middleware/auth.js
@@ -3,10 +3,10 @@ import JWT from 'jsonwebtoken'
 const authMiddleware = async (req,res,next) => {
     const { token } = req.headers;
     if (!token) {
-        return res.json({success:false,message:'Not Authorized Login Again'})
+        return res.json({success:false,message:'Not Authorized Login Again'});
     }
     try {
-        const token_decode = JWT.verify(token,process.env.JWT_SECRET)   //we will get token from users header which is written in above const \
+        const token_decode = JWT.verify(token,process.env.JWT_SECRET);   //we will get token from users header which is written in above const \
         req.body.userId = token_decode.id
         next()
     } catch (error) {
--- Food Del/backend/server.js
+++ Food Del/backend/server.js
@@ -1,16 +1,16 @@
-import express from "express"
 import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
+import express from "express"
+import { connectDB } from "./config/db.js"
 import cartRouter from "./routes/cartRoute.js"
+import foodRouter from "./routes/foodRoute.js"
 import orderRouter from "./routes/orderRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 
 // app - config
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json())  //whenever we will get the request from frontend to backend we will be parsing this using this json
